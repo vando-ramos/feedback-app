6d8c0af76ed9ab0dbdfc9c41c6da1b3cf7d84dee
vando <vandorramos@gmail.com>
2023-12-20 11:43:05 -0300

    Creating modal login part 1

--- feedback-app/src/components/modalLogin/index.vue
+++ feedback-app/src/components/modalLogin/index.vue
@@ -1,9 +1,73 @@
 <template>
-  <h1>Log in Modal</h1>
+  <div class="flex justify-between">
+    <h1 class="text-4xl font-black text-gray-800">
+      Log in
+    </h1>
+
+    <button @click="close" class="text-4xl text-gray-600 focus:outline-none">
+      &times;
+    </button>
+  </div>
+
+  <div class="mt-16">
+    <form @submit.prevent="handleSubmit">
+      <label class="block">
+        <span class="text-lg font-medium text-gray-800">Email</span>
+        <input v-model="state.email.value" type="email" :class="{'border-brand-danger': !!state.email.errorMessage}"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded" placeholder="email@email.com"
+        >
+        <span v-if="!!state.email.errorMessage" class="block font-medium text-brand-danger">
+          {{ state.email.errorMessage }}
+        </span>
+      </label>
+
+      <label class="block">
+        <span class="text-lg font-medium text-gray-800">Password</span>
+        <input v-model="state.password.value" type="password" :class="{'border-brand-danger': !!state.email.errorMessage}"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded" placeholder=""
+        >
+        <span v-if="!!state.password.errorMessage" class="block font-medium text-brand-danger">
+          {{ state.password.errorMessage }}
+        </span>
+      </label>
+
+      <button :disabled="state.isLoading" type="submit" :class="{'opacity-50': state.isLoading}"
+        class="px-8 py-3 mt-10 text-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none transition-all duration-150"
+      >
+        Log in
+      </button>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {
+import { reactive } from 'vue'
+import useModal from '../../hooks/useModal'
 
+export default {
+  setup () {
+    const modal = useModal()
+
+    const state = reactive({
+      hasErrors: false,
+      isLoading: false,
+      email: {
+        value: '',
+        errorMessage: ''
+      },
+      password: {
+        value: '',
+        errorMessage: ''
+      }
+    })
+
+    // function handleSubmit () {
+    // }
+
+    return {
+      state,
+      close: modal.close
+    }
+  }
 }
 </script>
--- feedback-app/src/views/home/index.vue
+++ feedback-app/src/views/home/index.vue
@@ -2,19 +2,19 @@
   <CustomHeader @signup='handleSignup' @login='handleLogin' />
   <Contact />
   <div class="flex justify-center py-10 bg-brand-gray">
-    <p class="font-medium text-center text-gray-800">Feedback-app © 2023</p>
+    <p class="font-medium text-center text-gray-800">Feedbacker © 2023</p>
   </div>
 </template>
 
 <script>
 import { onMounted } from 'vue'
 import { useRouter } from 'vue-router'
-import Contact from './Contact.vue'
 import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
 import useModal from '../../hooks/useModal'
 
 export default {
-  components: { Contact, CustomHeader },
+  components: { CustomHeader, Contact },
   setup () {
     const router = useRouter()
     const modal = useModal()
@@ -28,13 +28,13 @@ export default {
 
     function handleLogin () {
       modal.open({
-        components: 'modalLogin'
+        component: 'modalLogin'
       })
     }
 
     function handleSignup () {
       modal.open({
-        components: 'modalSignup'
+        component: 'modalSignup'
       })
     }
 
