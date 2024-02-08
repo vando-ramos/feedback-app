0fddfefcbcd6db9eef19b4af84d0570d8e7821ba
vando <vandorramos@gmail.com>
2024-02-08 18:18:31 -0300

    Creating registration modal

--- feedback-app/src/components/modalSignup/index.vue
+++ feedback-app/src/components/modalSignup/index.vue
@@ -1,9 +1,151 @@
 <template>
-  <h1>Sign up Modal</h1>
+  <div class="flex justify-between">
+    <h1 class="text-4xl font-black text-gray-800">
+      Sign up
+    </h1>
+
+    <button @click="close" class="text-4xl text-gray-600 focus:outline-none">
+      &times;
+    </button>
+  </div>
+
+  <div class="mt-16">
+    <form @submit.prevent="handleSubmit">
+      <label class="block">
+        <span class="text-lg font-medium text-gray-800">Name</span>
+        <input v-model="state.name.value" type="text" :class="{'border-brand-danger': !!state.name.errorMessage}"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded" placeholder="Enter your name"
+        >
+        <span v-if="!!state.name.errorMessage" class="block font-medium text-brand-danger">
+          {{ state.name.errorMessage }}
+        </span>
+      </label>
+
+      <label class="block mt-9">
+        <span class="text-lg font-medium text-gray-800">Email</span>
+        <input v-model="state.email.value" type="email" :class="{'border-brand-danger': !!state.email.errorMessage}"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded" placeholder="email@email.com"
+        >
+        <span v-if="!!state.email.errorMessage" class="block font-medium text-brand-danger">
+          {{ state.email.errorMessage }}
+        </span>
+      </label>
+
+      <label class="block mt-9">
+        <span class="text-lg font-medium text-gray-800">Password</span>
+        <input v-model="state.password.value" type="password" :class="{'border-brand-danger': !!state.email.errorMessage}"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded" placeholder="Enter Password"
+        >
+        <span v-if="!!state.password.errorMessage" class="block font-medium text-brand-danger">
+          {{ state.password.errorMessage }}
+        </span>
+      </label>
+
+      <button :disabled="state.isLoading" type="submit" :class="{'opacity-50': state.isLoading}"
+        class="px-8 py-3 mt-10 text-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none transition-all duration-150"
+      >
+        <icon v-if="state.isLoading" name='loading' class="animate-spin" />
+        <span v-else>Sign up</span>
+      </button>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {
+import { reactive } from 'vue'
+import { useRouter } from 'vue-router'
+import { useField } from 'vee-validate'
+import { useToast } from 'vue-toastification'
+import useModal from '../../hooks/useModal'
+import icon from '../icon'
+import { validateEmptyAndLength3, validateEmptyAndEmail } from '../../utils/validators'
+import services from '../../services'
 
+export default {
+  components: { icon },
+  setup () {
+    const router = useRouter()
+    const modal = useModal()
+    const toast = useToast()
+
+    const {
+      value: nameValue,
+      errorMessage: nameErrorMessage
+    } = useField('name', validateEmptyAndLength3)
+
+    const {
+      value: emailValue,
+      errorMessage: emailErrorMessage
+    } = useField('email', validateEmptyAndEmail)
+
+    const {
+      value: passwordValue,
+      errorMessage: passwordErrorMessage
+    } = useField('password', validateEmptyAndLength3)
+
+    const state = reactive({
+      hasErrors: false,
+      isLoading: false,
+      name: {
+        value: nameValue,
+        errorMessage: nameErrorMessage
+      },
+      email: {
+        value: emailValue,
+        errorMessage: emailErrorMessage
+      },
+      password: {
+        value: passwordValue,
+        errorMessage: passwordErrorMessage
+      }
+    })
+
+    async function login ({ email, password }) {
+      const { data, errors } = await services.auth.login({ email, password })
+      if (!errors) {
+        window.localStorage.setItem('token', data.token)
+        router.push({ name: 'feedbacks' })
+        modal.close()
+      }
+
+      state.isLoading = false
+    }
+
+    async function handleSubmit () {
+      try {
+        toast.clear()
+        state.isLoading = true
+        const { errors } = await services.auth.register({
+          name: state.name.value,
+          email: state.email.value,
+          password: state.password.value
+        })
+
+        if (!errors) {
+          await login({
+            email: state.email.value,
+            password: state.password.value
+          })
+          return
+        }
+
+        if (errors.status === 400) {
+          toast.error('Sign up error!')
+        }
+
+        state.isLoading = false
+      } catch (error) {
+        state.isLoading = false
+        state.hasErrors = !!error
+        toast.error('Sign up error!')
+      }
+    }
+
+    return {
+      state,
+      close: modal.close,
+      handleSubmit
+    }
+  }
 }
 </script>
